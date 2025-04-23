4bd786c3a8b65a9f388a12919167685ebede0d8d
Victor <victor.gitonga1@student.moringaschool.com>
2025-04-23 11:20:02 +0300

    routes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,16 @@
+// src/components/NavBar.jsx
+import React from "react";
 import { NavLink } from "react-router-dom";
-import "./NavBar.css";
+import "./NavBar.css"; // Optional if you’re using CSS
 
 function NavBar() {
   return (
-    <nav className="navbar">
-    
+    <nav>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/directors">Directors</NavLink>
+      <NavLink to="/actors">Actors</NavLink>
     </nav>
-    );
-};
+  );
+}
 
 export default NavBar;
